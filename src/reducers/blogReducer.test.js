458bcafa47100bd4fbe9bc6752ec6d4f90aedce8
Perry Sittser <sittserp@gmail.com>
2021-02-17 16:35:59 -0800

    blog reducer DELETE test passes

--- src/reducers/blogReducer.test.js
+++ src/reducers/blogReducer.test.js
@@ -16,6 +16,7 @@ describe('blog reducer', () => {
             and he got to keep one! That's how he got his puppy, Pal.`
     });
 
+
     const newState = reducer(state, action);
 
     expect(newState).toEqual({
@@ -30,4 +31,26 @@ describe('blog reducer', () => {
       }]
     });
   });
+
+  it('deletes a blog w/ DELETE_BLOG action', () => {
+    const state = {
+      blogs: [{
+
+        title: 'My Dog Pal',
+        body: 
+          `Arthur was so excited when he started his pet business 
+          watching other people's animals! When he watched Perky, 
+          he had a hard time taking care of her, but then she had puppies, 
+          and he got to keep one! That's how he got his puppy, Pal.`
+      }]
+    };
+
+    const action = deleteBlog('My Dog Pal');
+
+    expect(reducer(state, action)).toEqual({
+      blogs: []
+      
+    });
+  });
 });
+
