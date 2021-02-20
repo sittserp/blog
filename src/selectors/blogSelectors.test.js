92613d45c0f4caf6bb7b48aed00c7b9d132b0152
Perry Sittser <sittserp@gmail.com>
2021-02-19 16:36:59 -0800

    blogReducer test passing

--- src/selectors/blogSelectors.test.js
+++ src/selectors/blogSelectors.test.js
@@ -3,15 +3,17 @@ import { getBlogs } from './blogSelectors';
 describe('blog selectors', () => {
   it('selects the list of blogs from state', () => {
     const state = {
-      blogs: [{
+      blogs: {
+        blogs: [{
       
-        title: 'My Dog Pal',
-        body: 
+          title: 'My Dog Pal',
+          body: 
             `Arthur was so excited when he started his pet business 
             watching other people's animals! When he watched Perky, 
             he had a hard time taking care of her, but then she had puppies, 
             and he got to keep one! That's how he got his puppy, Pal.`
-      }]
+        }]
+      }
     };
         
     const blogs = getBlogs(state);
